bebe8c5b3e10f2c42da3c0e6b62c1673d6e25b6f
sandman633 <69395587+sandman633@users.noreply.github.com>
2022-02-16 00:44:11 +0300

    Добавьте файлы проекта.

---
+++ University.Web/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Web.Models;
+public class Course
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int CourseID { get; set; }
+    public string Title { get; set; }
+    public int Credits { get; set; }
+
+    public ICollection<Enrollment> Enrollments { get; set; }
+}
